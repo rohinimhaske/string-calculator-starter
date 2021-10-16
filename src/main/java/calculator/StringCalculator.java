4a0983800fff49ce4bd199538d50e5e5c026f4d8
Rohini Mhaske <84780059+rohinimhaske@users.noreply.github.com>
2021-10-17 04:40:42 +0530

    Code changes for string with new line

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,7 +1,7 @@
 package calculator;
 
 class StringCalculator {
-    private static final String regex = "[,]";
+    private static final String regex = "[\n,]";
 
    public static int add(String input) {
 		if (input.isEmpty()) {
