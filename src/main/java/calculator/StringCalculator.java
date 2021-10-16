015ed9120d16b9c0477578eb189df2f49fdb08f4
Rohini Mhaske <84780059+rohinimhaske@users.noreply.github.com>
2021-10-17 04:52:56 +0530

    Support different delimiters

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,7 +1,7 @@
 package calculator;
 
 class StringCalculator {
-    private static final String regex = "[\n,]";
+    private static final String regex = "[\n,;]";
 
    public static int add(String input) {
 		if (input.isEmpty()) {
@@ -11,7 +11,11 @@ class StringCalculator {
 		String[] numbers = input.split(regex);
 		int sum = 0;
 		for (String num : numbers) {
-			sum = sum + Integer.parseInt(num);
+			try {
+				sum = sum + Integer.parseInt(num);
+			} catch (NumberFormatException nfe) {
+				nfe.getMessage();
+			}
 		}
 		return sum;
 	}
