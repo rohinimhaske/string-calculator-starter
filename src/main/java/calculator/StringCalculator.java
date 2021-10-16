238eaac3d93828ac27754f430c9e712073b7ae2b
Rohini Mhaske <84780059+rohinimhaske@users.noreply.github.com>
2021-10-17 04:23:28 +0530

    Changes for empty,single digit,multiple numbers string

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,9 +1,19 @@
 package calculator;
 
 class StringCalculator {
+    private static final String regex = "[,]";
 
-    public int add(String input) {
-        return 0;
-    }
+   public static int add(String input) {
+		if (input.isEmpty()) {
+			return 0;
+		}
+
+		String[] numbers = input.split(",");
+		int sum = 0;
+		for (String num : numbers) {
+			sum = sum + Integer.parseInt(num);
+		}
+		return sum;
+	}
 
 }
