ac1a513a31605b92d79d4123a48c10c93bbc7692
Rohini Mhaske <84780059+rohinimhaske@users.noreply.github.com>
2021-10-17 04:58:53 +0530

    throw an exception “negatives not allowed” & print them

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,19 +3,29 @@ package calculator;
 class StringCalculator {
     private static final String regex = "[\n,;]";
 
-   public static int add(String input) {
+    public static int add(String input) throws Exception {
 		if (input.isEmpty()) {
 			return 0;
 		}
 
 		String[] numbers = input.split(regex);
 		int sum = 0;
+		List<Integer> negativeNumbers = new ArrayList<>();
 		for (String num : numbers) {
 			try {
-				sum = sum + Integer.parseInt(num);
+				int number = Integer.parseInt(num);
+				if (number >= 0) {
+					sum = sum + number;
+				} else {
+					negativeNumbers.add(number);
+				}
 			} catch (NumberFormatException nfe) {
 				nfe.getMessage();
 			}
+		}
+
+		if (!negativeNumbers.isEmpty()) {
+			throw new Exception("negatives not allowed " + negativeNumbers);
 		}
 		return sum;
 	}
